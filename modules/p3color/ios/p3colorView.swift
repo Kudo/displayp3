70c92d83db23a36a39f93556e31a4dcee3a88e6d
Kudo Chien <kudo@expo.dev>
2023-10-31 22:37:44 +0800

    fallback rgb color if the screen is not supported

--- modules/p3color/ios/p3colorView.swift
+++ modules/p3color/ios/p3colorView.swift
@@ -5,6 +5,10 @@ import ExpoModulesCore
 class p3colorView: ExpoView {
   required init(appContext: AppContext? = nil) {
     super.init(appContext: appContext)
-    backgroundColor = UIColor.init(displayP3Red: 1.0, green: 0, blue: 0, alpha: 1.0)
+    if self.traitCollection.displayGamut == .P3 {
+      backgroundColor = UIColor.init(displayP3Red: 1.0, green: 0, blue: 0, alpha: 1.0)
+    } else {
+      backgroundColor = UIColor.init(red: 1.0, green: 0, blue: 0, alpha: 1.0)
+    }
   }
 }
